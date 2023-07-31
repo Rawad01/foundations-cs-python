aa3ea877aa4d416c68ebf0003b05252cfcaa85c7
Rawad01 <92724617+Rawad01@users.noreply.github.com>
2023-07-31 17:21:14 +0300

    user ticket booking

--- midterm-project/main.py
+++ midterm-project/main.py
@@ -3,6 +3,7 @@
 ###################
 
 from collections import Counter
+import datetime
 
 #########
 # Admin:
@@ -13,27 +14,23 @@ def displayAdminMenu():
     print("1. Display Statistics\n2. Book a Ticket\n3. Display all Tickets\n4. Change Ticket’s Priority\n5. Disable Ticket\n6. Run Events\n7. Exit")
 
 def displayStatistics(matrix):
+    # Worst case: O(N)
     event_list = []
     for row in range(len(matrix)):
         event_list.append(matrix[row][1])
     event_list = Counter(event_list)
-    return event_list.most_common(1)[0][0]# Got this method by researching on google. It gives you the most frequent element in a list.
+    return event_list.most_common(1)[0][0]# Got this method by researching on google(geeksforgeeks). It gives you the most frequent element in a list.
 
 def adminBookTicket(username, event_id, date_of_event, priority):
     tickets_file = open("tickets.txt", "a")# a for appending in order to insert after the last line.
-    tickets_matrix = importTickets()
-    tickets_id = []
-    for row in range(len(tickets_matrix)):
-        tickets_id.append(tickets_matrix[row][0])
-    last_id = tickets_id[len(tickets_id) - 1]
-    temp_id = last_id[4:]
-    temp_id = int(temp_id) + 1
-    tick_id = "tick" + str(temp_id)
-    tickets_file.write("\n" + tick_id + ", " + username + ", " + event_id + ", " + date_of_event + ", " + str(priority))
+    # autoincrementing the ticket id and then writing the ticket values into the file
+    tick_id = incrementTicketId()
+    tickets_file.write("\n" + tick_id + ", " + event_id + ", " + username + ", " + date_of_event + ", " + str(priority))
     tickets_file.close()
 
 def displayAllTickets():
-    print("i am in admin display tickets")
+    date = datetime.datetime.now()
+    date = date.strftime("%Y%m%d")# Got this method by researching on google(https://www.w3schools.com/python/python_datetime.asp).
 
 def changeTicketPriority():
     print("i am in admin change ticket priority")
@@ -52,14 +49,19 @@ def displayUserMenu():
     print("User menu:")
     print("1. Book a ticket\n2. Exit")
 
-def bookTicket():
-    print("i am in ticket booking")
+def bookTicket(username, event_id, date_of_event):
+    tickets_file = open("tickets.txt", "a")
+    def_priority = 0
+    tick_id = incrementTicketId()
+    tickets_file.write("\n" + tick_id + ", " + event_id + ", " + username + ", " + date_of_event + ", " + str(def_priority))
+    tickets_file.close()
 
 ##################
 # Common methods:
 ##################
 
 def importTickets():
+    # Worst case: O(N + N)
     # Opening the file and reading number of lines
     tickets = []
     tickets_matrix = []
@@ -70,13 +72,24 @@ def importTickets():
     # appending the content into a list
     # then changing the list into a matrix of tickets
     tickets_file = open("tickets.txt", "r")
-    for line in tickets_file:
+    for line in tickets_file: # Worst case: O(N) n number of lines
         tickets.append(line.strip("\n"))
 
-    for row in range(num_lines):
+    for row in range(num_lines): # Worst case: O(N) n rows
         tickets_matrix.append(tickets[row].split(","))
     return tickets_matrix
 
+def incrementTicketId(): # this method is for autoincrementing the ticket id
+    # Worst case: O(N)
+    tickets_matrix = importTickets()
+    tickets_id = []
+    for row in range(len(tickets_matrix)):
+        tickets_id.append(tickets_matrix[row][0])
+    last_id = tickets_id[len(tickets_id) - 1]
+    temp_id = last_id[4:]
+    temp_id = int(temp_id) + 1
+    tick_id = "tick" + str(temp_id)
+    return tick_id
 ########
 # Main:
 ########
@@ -114,7 +127,9 @@ def main():
     if(username != "admin"): # chosing from user menu
         while(choice != 2):
             if(choice == 1):
-                bookTicket()
+                event_id = input("Please insert the event ID for the ticket: ")
+                date_of_event = input("Please insert the date of the event in the following format(YYYYMMDD): ")
+                bookTicket(username, event_id, date_of_event)
             else:
                 print("Wrong choice please choose one of the below choices: ")
             choice = eval(input("Please choose a number: "))
@@ -152,7 +167,7 @@ def main():
                     print("Back to admin menu:")
                     print()
             elif(choice == 3):
-                displayAllTickets()
+                print(displayAllTickets())
             elif(choice == 4):
                 changeTicketPriority()
             elif(choice == 5):
